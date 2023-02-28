f55adc28bae84c37229933edac7e2f8ff6df80c1
amjdp <athiramj123@gmail.com>
2023-02-28 10:58:04 +0530

    Fix : add rendering objects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 
 const content = "Cybersquare professional";
+const obj = {"title":"Cybersquare", "name":"baabtra"};
+const courses = {"course1":"python","course2":"angular", "course3":"react"}
 
 function App(){
  return(
-   <div>
+   <div className='App'>
      <h1>{content}</h1>
-     <p>content</p>
+     <p>{obj.title} courses provided by {obj.name}</p>
+     <h3>{courses.course1}</h3>  
+     <h3>{courses.course2}</h3> 
+     <h3>{courses.course3}</h3> 
    </div>
  );
 }
-
 
 export default App;
