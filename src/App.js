1b304e167070b89f20cb71307ba4c61213d3363d
amjdp <athiramj123@gmail.com>
2023-02-28 10:50:30 +0530

    rendering objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
 const content = "Cybersquare professional";
+const obj ={"title":"Cybersquare", "name":"baabtra"};
+const courses = {"course1":"python", "course2":"angular", "course3":"react"}
 
 function App(){
  return(
    <div>
      <h1>{content}</h1>
-     <p>content</p>
+     <p>{obj.title} courses provided by {obj.name}</p>
+     <h3>{courses.course1}</h3>
+     <h3>{courses.course2}</h3>
+     <h3>{courses.course3}</h3>
    </div>
  );
 }
